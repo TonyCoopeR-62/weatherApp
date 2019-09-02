308d4d25707e06a0bb3362f1977d6f42d88385da
TonyCoopeR <jar1991@mail.ru>
2019-09-02 17:17:16 +0300

    in progress

--- script.js
+++ script.js
@@ -6,3 +6,19 @@
 //   icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
 //   temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
 //   humidity.innerHTML = jsonObject.main.humidity + "%";
+
+let searchText = document.getElementById('search').innerHTML;
+let apikey = "8a103589621e12d36e9ffd2d7ee39848";
+let link = "https://api.openweathermap.org/data/2.5/weather?q=" + searchText + "&appid=" + apikey;
+
+document.getElementById('button').addEventListener('click', getWeather);
+
+function getWeather() {
+    fetch(link)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+    });
+}
