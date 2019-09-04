455f1c96cc6c8293309bf5b5f7401a78f6c73e0b
TonyCoopeR <jar1991@mail.ru>
2019-09-04 12:07:02 +0300

    ready at 90%, but yet in progress

--- script.js
+++ script.js
@@ -1,24 +1,69 @@
 //   openweathermap-api 8a103589621e12d36e9ffd2d7ee39848
-//   sample url https://api.openweathermap.org/data/2.5/weather?q=
-// 
-//   let jsonObject = JSON.parse(response);
-//   cityName.innerHTML = jsonObject.name;
-//   icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
-//   temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
-//   humidity.innerHTML = jsonObject.main.humidity + "%";
 
-document.getElementById('button').addEventListener('click', getWeather);
+let apiKey = "8a103589621e12d36e9ffd2d7ee39848";
+let searchButton = document.getElementById("button");
+let searchInput = document.getElementById("text");
+let cityName = document.getElementById("city-name");
+let icon = document.getElementById("icon");
+let temperature = document.getElementById("temper");
+let humidity = document.getElementById("humidity");
 
-function getWeather() {
-    let searchText = document.getElementById('text').value;
-    let apikey = "8a103589621e12d36e9ffd2d7ee39848";
-    let link = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${apikey}`;
-    fetch(link)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(myJson) {
-        console.log(JSON.stringify(myJson));
-        console.log(searchText);
-    });
+searchButton.addEventListener('click', getWeather);
+searchInput.addEventListener('keyup', enterPressed);
+
+function enterPressed (event) {
+    if (event.key === "Enter") {
+        getWeather();
+    }
 }
+
+function getWeather () {
+    if (searchInput.value === "") {
+        alert('input city to search')
+    } else {
+        let link = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}`;
+    httpRequestAsync(link, theResponse);
+    }
+}
+
+function theResponse (response) {
+    let dataJson = JSON.parse(response);
+    cityName.innerHTML = dataJson.name;
+    icon.src = "http://openweathermap.org/img/w/" + dataJson.weather[0].icon + ".png";
+    temperature.innerHTML = parseInt(dataJson.main.temp - 273) + "&deg";
+    humidity.innerHTML = dataJson.main.humidity + "%";
+}
+
+function httpRequestAsync(url, callback) {
+    console.log("start req");
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => { 
+        if (httpRequest.readyState == 4 && httpRequest.status == 200)
+            callback(httpRequest.responseText);
+    }
+    httpRequest.open("GET", url, true); 
+    httpRequest.send();
+}
+
+
+
+
+// document.getElementById('button').addEventListener('click', getWeather);
+// let icon = document.getElementById('icon');
+// let cityName = document.getElementById('cityName').innerHTML;
+// function getWeather() {
+    
+//     let searchText = document.getElementById('text').value;
+//     let apikey = "8a103589621e12d36e9ffd2d7ee39848";
+//     let link = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${apikey}`;
+//     fetch(link)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(myJson) {
+//         let apiData = (JSON.stringify(myJson));
+//         cityName = apiData.name;
+//         //icon.src = "http://openweathermap.org/img/w/" + apiData.weather[0].icon + ".png";
+//     });
+// }
+
