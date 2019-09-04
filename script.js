574cd1c70d97ee52773f012407db75c3e28dee8d
TonyCoopeR <jar1991@mail.ru>
2019-09-04 10:07:34 +0300

    fix previous problem with id.value. It was stupid fail))

--- script.js
+++ script.js
@@ -7,18 +7,18 @@
 //   temperature.innerHTML = parseInt(jsonObject.main.temp - 273) + "°";
 //   humidity.innerHTML = jsonObject.main.humidity + "%";
 
-let searchText = document.getElementById('text').value;
-let apikey = "8a103589621e12d36e9ffd2d7ee39848";
-let link = "https://api.openweathermap.org/data/2.5/weather?q=" + searchText + "&appid=" + apikey;
-
 document.getElementById('button').addEventListener('click', getWeather);
 
 function getWeather() {
+    let searchText = document.getElementById('text').value;
+    let apikey = "8a103589621e12d36e9ffd2d7ee39848";
+    let link = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${apikey}`;
     fetch(link)
     .then(function(response) {
         return response.json();
     })
     .then(function(myJson) {
         console.log(JSON.stringify(myJson));
+        console.log(searchText);
     });
 }
